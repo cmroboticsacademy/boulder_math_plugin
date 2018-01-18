7b507c0ff6ddf28a7b1d9eedce4e24ca17f51a69
Joshua Jarvis <Joshua Jarvis>
2018-01-18 13:55:32 -0500

    Working port of boulder math.

--- app/assets/javascripts/boulder_math_plugin/application.js
+++ app/assets/javascripts/boulder_math_plugin/application.js
@@ -9,5 +9,29 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require ./phaser/library/phaser.min.js
+//= require ./phaser/library/phaser-checkbox/phaser.checkbox.min.js
+
+//= require ./phaser/boulder_math/states/Boot.js
+//= require ./phaser/boulder_math/states/Preload.js
+//= require ./phaser/boulder_math/states/MainMenu.js
+//= require ./phaser/boulder_math/states/Game.js
+//= require ./phaser/boulder_math/states/LoseScene.js
+//= require ./phaser/boulder_math/states/Settings.js
+//= require ./phaser/boulder_math/states/WinScene.js
+
+//= require ./phaser/boulder_math/prefabs/truck.js
+//= require ./phaser/boulder_math/prefabs/arrow.js
+//= require ./phaser/boulder_math/prefabs/lift.js
+//= require ./phaser/boulder_math/prefabs/popupModal.js
+//= require ./phaser/boulder_math/prefabs/scoreMeter.js
+//= require ./phaser/boulder_math/prefabs/controlPanel.js
+//= require ./phaser/boulder_math/prefabs/levelLayout.js
+//= require ./phaser/boulder_math/prefabs/challenge.js
+//= require ./phaser/boulder_math/prefabs/radioButtons.js
+//= require ./phaser/boulder_math/prefabs/dust.js
+
+//= require ./phaser/node_modules/@orange-games/phaser-input/build/phaser-input.js
+//= require ./phaser/boulder_math.js
 //= require_tree .
