ca48692cfd51ee76bfdc2816d77cf70a7d090ed2
Joshua Jarvis <Joshua Jarvis>
2018-01-18 20:39:29 -0500

    Now referencing assets with rails helper. Moved assets to asset pipeline folder.

--- lib/boulder_math_plugin/engine.rb
+++ lib/boulder_math_plugin/engine.rb
@@ -1,11 +1,5 @@
 module BoulderMathPlugin
   class Engine < ::Rails::Engine
     isolate_namespace BoulderMathPlugin
-
-
-    # Initializer to combine this engines static assets with the static assets of the hosting site.
-    initializer "static assets" do |app|
-        app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-    end
   end
 end
